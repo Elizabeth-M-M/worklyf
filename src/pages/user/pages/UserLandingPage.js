a6f250ca01abf5f347d121fd7c03d571627932b8
Elizabeth <mwendemuthusi6@gmail.com>
2023-07-11 17:36:50 +0300

    Feat: add task cards to display task

--- src/pages/user/pages/UserLandingPage.js
+++ src/pages/user/pages/UserLandingPage.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import { ChevronLeftIcon, FilterIcon, PlayIcon, SearchIcon, UserIcon } from '../../../assets/icons';
+import { ChevronLeftIcon, ClockIcon, FilterIcon, PlayIcon, SearchIcon, UserIcon } from '../../../assets/icons';
 import Status from '../Status';
 import Category from '../Category';
+import { tasks } from '../../../assets/tasks';
 
 export default function UserLandingPage() {
+
   return (
     <div className="py-5 px-8 text-gray-lighter">
       <div className="flex align-center justify-between">
@@ -30,9 +32,42 @@ export default function UserLandingPage() {
           <FilterIcon />
         </div>
       </div>
-      <Status/>
-      <Category/>
-      
+      <Status />
+      <Category />
+      <div className="mt-6">
+        <div className="flex align-center justify-between">
+          <h4 className="font-bold tracking-wide text-xl pb-3">
+            Ongoing Tasks
+          </h4>
+          <p className="text-xs tracking-wide text-pink-light cursor-pointer">See all</p>
+        </div>
+        {tasks.slice(0, 2).map((task) => (
+          <div className=" bg-gray-light rounded-xl overflow-hidden cursor-pointer mb-2">
+            <div className="px-5 pt-4">
+              <div className="flex align-center justify-between mb-3">
+                <h4 className="font-bold tracking-wide text-sm">
+                  Complete tasks
+                </h4>
+                <p className="bg-pink-light p-1 rounded-xl text-xs text-white ">
+                  urgency
+                </p>
+              </div>
+              <span className="bg-pink-light p-1 rounded-xl text-xs text-white  text-center">
+                category
+              </span>
+
+              <div>
+                <div className="flex align-center justify-start mt-3">
+                  <ClockIcon />
+                  <p className="text-xs tracking-wide ms-2 mb-2">
+                    Due in 3 days: 12/03/2023
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
