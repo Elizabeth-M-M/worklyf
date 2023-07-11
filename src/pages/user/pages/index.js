3b6e9a933d8442199e9a8c82cf29e75a54d82f7a
Elizabeth <mwendemuthusi6@gmail.com>
2023-07-11 13:43:19 +0300

    Feat: added icons for the project

--- src/pages/user/pages/index.js
+++ src/pages/user/pages/index.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function UserLandingPage() {
   return (
-    <div>UserLandingPage</div>
+    <div>
+     <div>
+      <h4>Hi, Liz</h4>      
+      <p>10 tasks pending</p>
+      </div> 
+      
+    </div>
   )
 }
