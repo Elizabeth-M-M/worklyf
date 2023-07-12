c2ea6063befb236aaa1e03e8e05ee8d4cc95b9da
Elizabeth <mwendemuthusi6@gmail.com>
2023-07-12 10:05:29 +0300

    Style: grid on large screen

--- src/pages/user/pages/UserLandingPage.js
+++ src/pages/user/pages/UserLandingPage.js
@@ -12,7 +12,7 @@ export default function UserLandingPage() {
       <div className="flex align-center justify-between">
         <div>
           <h4 className="font-bold text-3xl tracking-wide">Hi Liz</h4>
-          <p className="text-pink-dark tracking-wide text-sm my-2 mb-5">
+          <p className="text-pink-dark tracking-wide text-sm my-2 mb-5 md:mb-0">
             4 tasks pending
           </p>
         </div>
@@ -20,31 +20,39 @@ export default function UserLandingPage() {
           <UserIcon />
         </div>
       </div>
-      <div className="flex align-center justify-between">
-        <div className="bg-gray-light rounded-full flex ps-4 py-1 items-center w-3/4">
-          <SearchIcon />
-          <input
-            type="text"
-            className="bg-gray-light rounded-full p-2 px-4 outline-none w-3/4"
-            placeholder="search"
-          />
-        </div>
+      <div className="md:grid grid-cols-2 gap-8">
         <div>
-          <FilterIcon />
+          <Status />
+          <Category />
         </div>
-      </div>
-      <Status />
-      <Category />
-      <div className="mt-6">
-        <div className="flex align-center justify-between">
-          <h4 className="font-bold tracking-wide text-xl pb-3">
-            Ongoing Tasks
-          </h4>
-          <p className="text-xs tracking-wide text-pink-light cursor-pointer">See all</p>
+
+        <div className="mt-6 md:mt-0">
+          <div className="flex align-center justify-between my-5 md:mb-12">
+            <div className="bg-gray-light rounded-full flex ps-4 py-1 items-center w-3/4">
+              <SearchIcon />
+              <input
+                type="text"
+                className="bg-gray-light  outline-none w-3/4"
+                placeholder="search"
+              />
+            </div>
+            <div>
+              <FilterIcon />
+            </div>
+          </div>
+          <div className="flex align-center justify-between">
+            <h4 className="font-bold tracking-wide text-xl pb-3">
+              Ongoing Tasks
+            </h4>
+            <p className="text-xs tracking-wide text-pink-light cursor-pointer">
+              See all
+            </p>
+          </div>
+
+          {tasks.slice(0, 3).map((task) => (
+            <TaskCard task={task} />
+          ))}
         </div>
-        {tasks.slice(0, 2).map((task) => (
-          <TaskCard task={task}/>
-        ))}
       </div>
     </div>
   );
