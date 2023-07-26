d70b6b65127817bdb6e20669ef6f26e232d3740f
Elizabeth <mwendemuthusi6@gmail.com>
2023-07-26 07:17:45 +0300

    Feat: added the user splice to the store

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import ModalSlice from "../features/modal/ModalSlice";
 import TaskSlice from "../features/tasks/TaskSlice";
+import UserSlice from "../features/user/UserSlice";
 
 export const store = configureStore({
   reducer:{
     modal:ModalSlice,
     task:TaskSlice,
+    user:UserSlice
   }
 })
