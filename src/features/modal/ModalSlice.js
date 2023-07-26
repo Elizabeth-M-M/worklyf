dc346c8d4a2688fb7a782de934baaac7c83792d9
Elizabeth <mwendemuthusi6@gmail.com>
2023-07-26 07:20:20 +0300

    Fix: changed the get tasks argument to take in two variables

--- src/features/modal/ModalSlice.js
+++ src/features/modal/ModalSlice.js
@@ -1,19 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
  const initialState= {
-  showCreateTask:false
+  showCreateTask:false,
+  showProfileMenu:false,
+  showTaskMenu:false
  }
 const modalSlice = createSlice({
   name: "modal",
   initialState,
   reducers: {
     openCreateTask: (state, action) => {
-
       state.showCreateTask = !state.showCreateTask;
     },
     closeCreateTask: (state, action) => {
       state.showCreateTask = !state.showCreateTask;
     },
+    displayProfileMenu: (state, action) => {
+      state.showProfileMenu = !state.showProfileMenu;
+    },
+    displayTaskMenu: (state, action) => {
+      state.showTaskMenu = !state.showTaskMenu;
+    },
   },
 });
-export const { openCreateTask, closeCreateTask } = modalSlice.actions;
+export const { openCreateTask, closeCreateTask, displayProfileMenu, displayTaskMenu } = modalSlice.actions;
 export default modalSlice.reducer;
